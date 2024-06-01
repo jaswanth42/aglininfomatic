f3b041b0fca22698414fa0ef36eb42e21982c66a
Bigcommute <unipool.co@gmail.com>
2024-06-01 11:44:10 +0530

    raw code added

--- aglininfomatics/src/App.jsx
+++ aglininfomatics/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav>
+        <div>
+            {/* <img src="./aglin/socialCollage.png" alt="Social College Logo"> */}
+            <ul>
+                <li>
+                    <a href="/"><b>Home</b></a>
+                </li>
+                <li>
+                    <a href="/stories"><b>Services</b></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" >Prceing Plans</a>
+                    <div class="dropdown-content">
+                        <a href="#">Essential</a>
+                        <a href="#">Custom</a>
+                        <a href="#">Premium</a>
+                    </div>
+               </li>
+                <li>
+                    <a href="/contacts"><b>Contact Us</b></a>
+                </li>
+                <li>
+                    <a href="/apply"><b>Sign Up</b></a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+      <div class="main-content">
+        <div class="banner">Welcome to Social Collage</div>
+        <h1><b>Unlock Your Digital Potential with Us</b></h1>
+        <p>Where Digital Dreams Take Shape! Empower your business or personal brand with cutting-edge digital marketing solutions tailored for today's dynamic landscape. Discover how we harness the latest technologies to amplify your online presence and unlock growth opportunities.</p>
+        <button class="button get-started"><b>Get Started</b></button>
+        <button class="button learn-more"><b>Learn More</b></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
